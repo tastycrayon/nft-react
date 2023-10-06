fb46d3ce8d224bae3e4609644e62218020b03f1c
tastycrayon <mosiur.404.rahman@gmail.com>
2023-10-06 23:51:07 +0600

    -

--- src/routes/ErrorPage.tsx
+++ src/routes/ErrorPage.tsx
@@ -9,10 +9,10 @@ const ErrorPage: React.FC<PropTypes> = () => {
     <div className="flex items-center justify-center min-h-screen">
       <div className="flex flex-col">
         {/* Error Container  */}
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center p-4 text-center">
           <div className="text-secondary font-bold text-7xl">404</div>
 
-          <div className="font-bold text-3xl xl:text-7xl lg:text-6xl md:text-5xl mt-10">
+          <div className="font-bold text-2xl xl:text-7xl lg:text-5xl md:text-4xl mt-10">
             This page does not exist
           </div>
 
@@ -20,7 +20,7 @@ const ErrorPage: React.FC<PropTypes> = () => {
             The page you are looking for could not be found.
           </div>
           <Link to={HOME_PATH}>
-            <button className="btn btn-neutral mt-4">Return Home</button>
+            <button className="btn btn-primary mt-4">Return Home</button>
           </Link>
         </div>
       </div>
